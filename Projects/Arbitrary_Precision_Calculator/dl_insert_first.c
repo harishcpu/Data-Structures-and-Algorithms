5658537c95d126773fe35f8ba5e25f8cd34703d4
harish :) <46072258+HarishKumar-G@users.noreply.github.com>
2019-04-01 16:33:02 +0530

    Data-Structures Projects

--- Projects/Arbitrary_Precision_Calculator/dl_insert_first.c
+++ Projects/Arbitrary_Precision_Calculator/dl_insert_first.c
@@ -27,3 +27,75 @@ int dl_insert_first(node **head, node **tail, data_t data)
         *head = new;
         return SUCCESS;
 }
+
+/* ---------------------------------------------------------------------------- 
+ *   Author         : HARISH
+ *   Date           : Mon Jan 28
+ *   File           : insert_first.c
+ *   Title          : Function to insert the new node in the begining
+ *   Description    : This function inserts a new node at begining of the 
+                        existing linked list. Returns SUCCESS on success.
+ * ---------------------------------------------------------------------------- */
+#include "hahaha.h"
+
+int insert_first(node **head, data_t data)
+{
+    node *new = (node *)malloc(sizeof(node));
+
+    /* Check if new node is created */
+    if (new == NULL)
+        return FAILURE;
+
+    /* Fill the parts of the node */
+    new->data = data;
+    new->next = NULL;
+
+    /* If list is empty */
+    if (*head == NULL)
+    {
+        *head = new;
+        return SUCCESS;
+    }
+    else
+    {
+        new->next = *head;
+        *head = new;
+        return SUCCESS;
+    }
+}
+
+/* ---------------------------------------------------------------------------- 
+ *   Author         : HARISH
+ *   Date           : Mon Jan 28
+ *   File           : insert_at_first.c
+ *   Title          : Function to insert the new node in the begining
+ *   Description    : This function inserts a new node at begining of the 
+                        existing linked list. Returns SUCCESS on success.
+ * ---------------------------------------------------------------------------- */
+#include "hahaha.h"
+
+int sl_insert_first(node **head, data_t data)
+{
+    node *new = (node *)malloc(sizeof(node));
+
+    /* Check if new node is created */
+    if (new == NULL)
+        return FAILURE;
+
+    /* Fill the parts of the node */
+    new->data = data;
+    new->next = NULL;
+
+    /* If list is empty */
+    if (*head == NULL)
+    {
+        *head = new;
+        return SUCCESS;
+    }
+    else
+    {
+        new->next = *head;
+        *head = new;
+        return SUCCESS;
+    }
+}
