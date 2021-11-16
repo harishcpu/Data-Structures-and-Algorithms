7afbc80d496b168538aa5aace757f4fe156593d9
harishkumar101 <harishec031@gmail.com>
2021-11-17 00:15:45 +0530

    linked list delete node at a position

--- GFG_Java/LinkedLists/linkedListDelete.java
+++ GFG_Java/LinkedLists/linkedListDelete.java
@@ -23,6 +23,29 @@ public class linkedListDelete {
 		prevNode.next = temp.next;
 	}
 
+	public void deleteNodeAtPos(int position) {
+		if(head == null)
+			return;
+
+		if(position == 0) {
+			head = head.next;
+			return;
+		}
+
+		Node temp = head;
+		Node prev = head;
+		while(position != 0 && temp != null) {
+			prev = temp;
+			temp = temp.next;
+			position = position - 1;
+		}
+
+		if(temp == null)
+			return;
+
+		prev.next = temp.next;
+	}
+
 	public void insert(int data) {
 		Node newNode = new Node(data);
 		newNode.next = head;
@@ -45,14 +68,27 @@ public class linkedListDelete {
 	public static void main(String args[]) {
 		linkedListDelete llist = new linkedListDelete();
 		llist.insert(3);
+		llist.insert(4);
 		llist.insert(2);
 		llist.insert(1);
+		llist.insert(5);
+		llist.insert(6);
+		System.out.println("The linked list is ");
 		llist.printList();
+		System.out.println("Deleting node 2");
 		llist.deleteNode(2);
 		llist.printList();
+		System.out.println("Deleting node 3");
 		llist.deleteNode(3);
 		llist.printList();
+		System.out.println("Deleting node 1");
 		llist.deleteNode(1);
+		llist.printList();
+		System.out.println("Deleting node at position 1");
+		llist.deleteNodeAtPos(1);
+		llist.printList();
+		System.out.println("Deleting node at position 2");
+		llist.deleteNodeAtPos(2);
 		llist.printList();
 	}
 }
