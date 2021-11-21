5128600f8839b46e41a996b3532710de0d19cde3
harishkumar101 <harishec031@gmail.com>
2021-11-21 14:53:43 +0530

    check if a singly linked list is palindrome

---
+++ GFG_Java/LinkedLists/isPalindrome.java
@@ -0,0 +1,62 @@
+public class isPalindrome {
+	class Node {
+		int data;
+		Node next;
+		Node(int d) {data = d; next = null;}
+	}
+
+	Node head;
+
+	public void push(int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		newNode.next = head;
+		head = newNode;
+	}
+
+	public void printList(Node temp) {
+		while(temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+
+	Node left;
+	public boolean palindrome(Node right) {
+		left = head;
+		if(right == null)
+			return true;
+
+		boolean ispa = palindrome(right.next);
+		if(ispa == false)
+			return false;
+		
+		boolean ispal = (left.data == right.data);
+		left = left.next;
+		return ispal;
+	}
+	
+	public static void main(String args[]) {
+		isPalindrome llist = new isPalindrome();
+		llist.push(11);
+		llist.push(12);
+		llist.push(13);
+		llist.push(12);
+		llist.push(11);
+		llist.printList(llist.head);
+		if(llist.palindrome(llist.head))
+			System.out.println("Is palindrome");
+		else
+			System.out.println("Not a palindrome");
+		llist.push(16);
+		llist.printList(llist.head);
+		if(llist.palindrome(llist.head))
+			System.out.println("Is palindrome");
+		else
+			System.out.println("Not a palindrome");
+	}
+}
