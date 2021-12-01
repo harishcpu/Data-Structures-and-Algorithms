a410bb4017c88e87260c346e7ad29b93ac75d60b
harishkumar101 <harishec031@gmail.com>
2021-12-01 22:13:19 +0530

    Find correct position to insert a new element

--- leetcode/searchInsertPosition.c
+++ leetcode/searchInsertPosition.c
@@ -4,25 +4,24 @@ int searchInsert(int* nums, int numsSize, int target){
     int left, right, mid;
     left = 0;
     right = numsSize;
-    while(left < right) {
+    while(left <= right) {
         mid = left + (right - left) / 2;
         if(nums[mid] > target) {
             right = mid - 1;
-            if(nums[right] < target)
-                return mid-1;
         } else if (nums[mid] < target) {
             left = mid + 1;
-            if(nums[left] > target)
-                return mid+1;
         } else {
             return mid;
         }
     }
-    return -1;
+    return left;
 }
 
 int main(int argc, char *argv[]) {
 	int n[] = {1, 3, 5, 6};
-	printf("%d\n", searchInsert(n, 4, 7));
+	printf("target 7: %d\n", searchInsert(n, 4, 7));
+	printf("target 0: %d\n", searchInsert(n, 4, 0));
+	printf("target 2: %d\n", searchInsert(n, 4, 2));
+	printf("target 4: %d\n", searchInsert(n, 4, 4));
 	return 0;
 }
