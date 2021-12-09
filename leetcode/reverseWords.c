bec459ff265ed8248bb5369d2e3515ccaa5d1ea7
harishkumar101 <harishec031@gmail.com>
2021-12-09 15:13:15 +0530

    reverse words: accepted solution

---
+++ leetcode/reverseWords.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char *s, int i, int j) {
+    char temp;
+    while(i < j) {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+char *reverseWords(char *s) {
+    int i, j;
+    i = 0;
+    
+    if (s == NULL) return s;
+    while(i < strlen(s) && s[i] != ' ') {
+        j = i;
+        while(j < strlen(s) && s[j] != ' ') j++;
+        reverseString(s, i, j-1);
+        i = ++j;
+    }
+    return s;
+}
+int main(int argc, char *argv[]) {
+	char *s = "hello, world!";
+	printf("Before reversing: %s\n", s);
+	reverseWords(s);
+	printf("After reversing: %s\n", s);
+	return 0;
+}
